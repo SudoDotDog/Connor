4861f47513635457e69a2816beb079e87038e772
WMXPY <wm@mengw.io>
2019-03-29 16:16:10 -0500

    use index export pattern

--- src/declare.ts
+++ src/declare.ts
@@ -7,10 +7,6 @@
 import { ConnorAssert } from "./assert";
 import { ConnorError } from "./error";
 
-export interface IConnorDictionary {
-    [key: number]: string;
-}
-
+export type ConnorDictionary = Record<number, string>;
 export type ErrorCreationFunction = (symbol: number | string, ...replaces: string[]) => ConnorError;
-
 export type AssertCreationFunction = <T>(element: T) => ConnorAssert<T>;
